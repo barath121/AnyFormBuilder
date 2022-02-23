13575ffddb754f19cf471923f9fd13c9e5f0a9d5
barath121 <nadar.barath18@siesgst.ac.in>
2022-02-23 20:00:47 +0530

    Removed VUE for now

--- Client/anyform/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import Form from "./components/Form.vue";
-</script>
-
-<template>
-  <main>
-    <Form />
-  </main>
-</template>
--- Client/anyform/src/components/FormPagesContainer.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import FormPage from "./FormPage.vue";
-</script>
-<template>
-  <div class=" overflow-auto h-full borderTop colorWhite basis-11/12 ">
-    <FormPage/>
-</div>
-</template>
